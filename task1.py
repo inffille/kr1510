e274b9694692a57d0a8b35d4d3ed07a76aac7e54
Kostya Khmelevsky <khmelevskii.kn@phystech.edu>
2019-10-15 11:19:30 +0300

    task5 done

--- task1.py
+++ task1.py
@@ -1,6 +1,5 @@
-#Fix this file:
-# It should print your last name 10 times
-# and not mine
+# well ok
+# well well much better
 
-for i in range(5):
-	print('Shevchuk')
+for i in range(15):
+	print('Khmelevsky')
